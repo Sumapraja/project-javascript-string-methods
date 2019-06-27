6c912a1c0c3602a4cc27829e762323afa1a1029f
Sumapraja <irfansumapraja@gmail.com>
2019-06-27 13:29:43 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const MyName = `Irfan Risyad Sumapraja`
+console.log(MyName.length);
+console.log(MyName[7]);
+console.log(MyName.charAt(8));
+console.log(MyName[50]);
+console.log(MyName.charAt(50));
+for (let char of MyName) {
+    console.log(char);
+}
+const NewMyName = `9` + MyName[7]
+console.log(NewMyName);
+console.log(MyName.toUpperCase());
+console.log(MyName.toLowerCase());
+console.log(MyName.indexOf(`raja`));
+console.log(MyName.indexOf(`isya`));
+console.log(MyName.lastIndexOf(`raja`));
+console.log(MyName.includes(`fan`));
+console.log(MyName.includes(`u`));
+console.log(MyName.includes('i', 5));
+console.log(MyName.startsWith('I'));
+console.log(MyName.endsWith(`n`));
+console.log(`Hey
+Hello \\`);
+
+
+
+
+
+
+
+
+
+
+
+
